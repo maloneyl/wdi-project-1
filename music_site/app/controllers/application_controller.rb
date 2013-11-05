81bcf2a412154222d5b000bfb5a71c64c8d8c6ff
maloneyl <m@thinkmaloney.com>
2013-11-05 15:29:08 +0000

    added rescue so that logged-in user can delete his/her profile without triggering that current_user's 'id not found' problem

--- music_site/app/controllers/application_controller.rb
+++ music_site/app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue # what if current_user is still logged in when removing themself?
+      @current_user = nil
+    end
   end
 
   def logged_in?
